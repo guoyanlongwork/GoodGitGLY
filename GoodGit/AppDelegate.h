bfe45106e25fd9c5fa7ebc0b1c6c3ce51516b60d
dllo <guoqiaotian@126.com>
2016-05-12 16:17:58 +0800

    Initial Commit

---
+++ GoodGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoodGit
+//
+//  Created by dllo on 16/5/12.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
